a265f38bcd635b559afef895a264a9718494c5e4
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2021-02-16 13:53:43 +0000

    Remove outdated script

--- assets/scripts/ds-site.js
+++ assets/scripts/ds-site.js
@@ -1,21 +1,6 @@
 const dsSite = {
     init: () => {
-        const searchParams = new URLSearchParams(document.location.search);
 
-        if (searchParams.get('showall') === 'true') {
-            sessionStorage.setItem('showAll', true);
-        }
-
-        if (sessionStorage.getItem('showAll')) {
-            dsSite.showOutOfPhaseItems();
-        }
-    },
-
-    showOutOfPhaseItems: () => {
-        [].slice.call(document.querySelectorAll('.js-hidden-component')).forEach((item) => {
-            item.classList.remove('fully-hidden');
-            item.style.opacity = 0.5;
-        });
     }
 };
 
