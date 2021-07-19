9743680287e55349075822a776e4d5d3bb206966
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2021-07-19 08:14:40 +0100

    remove some inline JS

--- assets/scripts/ds-site.js
+++ assets/scripts/ds-site.js
@@ -3,6 +3,7 @@ import storage from './storage';
 const dsSite = {
     init: function () {
         this.setInitialCookiePermissions();
+        this.resizeIframes();
     },
 
     setInitialCookiePermissions: function () {
@@ -19,6 +20,18 @@ const dsSite = {
             );
         }
     },
+
+    resizeIframes: () => {
+        function resizeIframe(obj) {
+            obj.style.height = obj.contentWindow.document.body.scrollHeight + 2 + 'px';
+        }
+
+        const iframes = [].slice.call(document.querySelectorAll('iframe.example__iframe'));
+
+        iframes.forEach(iframe => {
+            iframe.addEventListener('load', () => resizeIframe(iframe));
+        });
+    }
 };
 
 dsSite.init();
