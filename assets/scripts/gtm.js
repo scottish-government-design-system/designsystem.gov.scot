cdfde0cda7a9bdf97a0597c2dea0788cb2c8a182
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2023-04-05 13:30:02 +0100

    MGS-7171 Design System- update cookie page to allow GA opt out

--- assets/scripts/gtm.js
+++ assets/scripts/gtm.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const gtmModule = {
     init: function () {
         window.dataLayer = window.dataLayer || [{}];
@@ -20,20 +21,43 @@ const gtmModule = {
         };
         xhr.ontimeout = () => {
             this.setUserType('timeout');
-            initGTM();
+            this.initGTM();
         };
         xhr.send(null);
     },
+
     initGTM: function () {
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        function getCookie(name) {
+            const cookie = {};
+            document.cookie.split(';').forEach(function (el) {
+                const [k, v] = el.split('=');
+                cookie[k.trim()] = v;
+            });
+            return cookie[name];
+        }
+
+        const gtmcode = document.getElementById('gtm-script').dataset.gtmcode;
+
+        let statisticsEnabled;
+        try {
+            statisticsEnabled = JSON.parse(atob(getCookie('cookiePermissions'))).statistics !== false;
+        } catch (err) {
+            statisticsEnabled = false;
+        }
+
+        if (statisticsEnabled) {
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5S47S9Z');
+            })(window, document, 'script', 'dataLayer', gtmcode);
+        }
     },
+
     setUserType: function(userType) {
         window.dataLayer[0].userType = userType;
     }
-}
+};
+
 gtmModule.init();
 export default gtmModule;
